c6d16f6e72f4b84fe45f0f9e861c6038aa8d8aaa
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2021-11-30 16:01:38 +0100

    Made Interface folder inside DataLayer project and added test interface

---
+++ DataLayer/Interfaces/IComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Interfaces
+{
+    public interface IComponent
+    {
+        public string type { get; set; }
+        public string price { get; set; }
+        public string weight { get; set; }
+
+        public static int amount;
+    }
+}
