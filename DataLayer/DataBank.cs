6dba0d7493babb7a2ef9e2f833b9a86a81db013a
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2021-11-30 17:10:44 +0100

    Created data handler, needs work(yikes)!

---
+++ DataLayer/DataBank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Entities;
+using DataLayer.Enums;
+using DataLayer.Interfaces;
+
+namespace PCPartPickerDataLayer
+{
+    public static class DataBank
+    {
+        static Customer customer;
+        static List<Order> orderList = new List<Order>();
+        public static Dictionary<string, List<int>> availableParts = new Dictionary<string, List<int>>();
+
+        public static void PopulateStore()
+        {
+            availableParts.Add("CPU", new List<int> {
+                (int)CpuEnums.CpuList.AMD_DecaCore,
+                (int)CpuEnums.CpuList.AMD_OctaCore,
+                (int)CpuEnums.CpuList.Intel_OctaCore,
+                (int)CpuEnums.CpuList.Intel_QuadCore});
+        }
+
+        public static void AddCustomer(string nameSurname, string address)
+        {
+            customer = new Customer(nameSurname, address);
+        }
+
+        public static Customer GetCustomer()
+        {
+            return customer;
+        }
+
+        public static void AddOrder(int cpu, int ram, int ramAmount, int pcCase)
+        {
+            Order order = new Order();
+            IComponent newCpu = new Cpu(cpu);
+            order.componentList.Add(newCpu);
+            orderList.Add(order);
+        }
+
+        public static List<Order> GetOrderList()
+        {
+            return orderList;
+        }
+    }
+}
---
+++ DataLayer/Enums/CpuEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Enums
+{
+    public class CpuEnums
+    {
+        public enum CpuList
+        {
+            AMD_DecaCore = 1,
+            AMD_OctaCore = 2,
+            Intel_OctaCore = 3,
+            Intel_QuadCore = 4
+        };
+    }
+}
